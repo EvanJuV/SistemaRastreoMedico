be657715dcba836f1a204da364701ce5d036b7ca
Nandotec <juan.fernando.ulloa@gmail.com>
2016-11-23 21:27:41 -0600

    Update Datos.java

--- src/java/modelos/Datos.java
+++ src/java/modelos/Datos.java
@@ -1,27 +1,45 @@
 package modelos;
+import java.sql.Date;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author Evan
- */
-class Datos {
-    
-    // Variables
-    private String fecha;
-    private String ubicacion;
+public class Datos {
+    PreparedStatement stmt;
     
     public Datos() {
         
     }
-
-    public Datos(String fecha, String ubicacion) {
-        this.fecha = fecha;
-        this.ubicacion = ubicacion;
+    
+    public void agregaDatos(int IdEquipo, int IdRastreo, String Ubicacion, Date Fecha, Connection con) {
+        try { 
+            String query = "INSERT INTO datos (IdEquipo, IdRastreo, Ubicacion, Fecha) VALUES (?, ?, ?, ?);
+            stmt = con.prepareStatement(query);
+            stmt.setInt(1, IdEquipo);
+            stmt.setInt(2, IdRastreo);
+            stmt.setString(3, Ubicacion);
+            stmt.setDate(4, Fecha);
+            stmt.execute();
+        }catch (SQLException e) { System.out.printIn ("No se pudo agregar los datos" + e ); }
+    }
+    
+    public Datos consultaDatos (int IdEquipo, int IdRastreo, Connection con){
+        try {
+            Datos datos = new Datos();
+            String query = "SELECT saldo FROM cuenta WHERE IdEquipo = ? AND IdRastreo = ?";
+            stmt = con.prepareStatement(query);
+            stmt.setInt(1, IdEquipo);
+            stmt.setInt(2, IdRastreo);
+            ResultSet rs = stmt.executeQuery();
+            rs.next();
+            datos.IdEquipo = rs.getInt("IdEquipo");
+            datos.IdRastreo = rs.getInt("IdRastreo");
+            datos.Ubicacion = rs.getString("Ubicacion");
+            datos.Fecha = rs.getDate("Fecha");
+            rs.close();
+            return datos;
+        }catch (Exception e) {System.out.println("No se pudo ejecutar consultaDatos() a la tabla Datos" + e); }
+        return null;
     }
 }
