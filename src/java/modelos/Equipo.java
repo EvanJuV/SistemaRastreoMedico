b7c272e45b1a05b0a0f3fc72629aeec8b7400a7d
Evan Juárez <e.juav24@gmail.com>
2016-11-23 19:45:01 -0600

    Métodos de equipo terminados

--- src/java/modelos/Equipo.java
+++ src/java/modelos/Equipo.java
@@ -1,5 +1,10 @@
 package modelos;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -13,31 +18,78 @@ package modelos;
 public class Equipo {
     
     // Variables
-    private String nombre;
-    private String estado;
-    private Datos datos;
-    private DispositivoRastreo rastreador;
+    int idEquipo;
+    String nombre;
+    String estado;
+    PreparedStatement stmt;
     
     public Equipo() {
         
     }
 
-    public Equipo(String nombre, String estado, Datos datos, DispositivoRastreo rastreador) {
+    public Equipo(String nombre, String estado) {
         this.nombre = nombre;
         this.estado = estado;
-        this.datos = datos;
-        this.rastreador = rastreador;
     }
     
     public void consultarEquipo() {
         
     }
 
-    public Datos getDatos() {
-        return datos;
+    public void crearEquipo(String nombreEquipo, Connection con) {
+        try {
+            String query = "INSERT INTO usuario (NombreUsuario) VALUES (?)";
+            stmt = con.prepareStatement(query);
+            stmt.setString(1, nombreEquipo);
+            stmt.execute();
+        }catch (Exception e) { System.out.println ("No se pudo ejecutar agregar() a la tabla Cliente" + e ); }
     }
+    
+    public Equipo consultarEquipo(int idEquipo, Connection con) {
+        try {
+            Equipo equipo = new Equipo();
+            String query = "SELECT * FROM equipo WHERE IDEquipo = ?";
+            stmt = con.prepareStatement(query);
+            stmt.setInt(1, idEquipo);
+            ResultSet rs = stmt.executeQuery();
+            rs.next(); 
+            equipo.idEquipo = rs.getInt("IdEquipo");
+            equipo.nombre = rs.getString("NombreEquipo");
+            equipo.estado = rs.getString("Estado");
+            rs.close();
+            
+            return equipo;
+        }catch (Exception e) {
+            System.out.println("No se pudo ejecutar consultarEquipo() a la tabla Equipo" + e);
+        }
+        
+        return null;
+    }  
+    
+    public ArrayList<Equipo> listarEquipos(Connection con) {
+        ArrayList<Equipo> equipos = new ArrayList<>();
+        
+        try {
+            String query = "SELECT * FROM equipo;";
+            stmt = con.prepareStatement(query);
+            ResultSet rs = stmt.executeQuery();
+            
+            while(rs.next()) {
+                Equipo equipo = new Equipo();
+                equipo.idEquipo = rs.getInt("IdEquipo");
+                equipo.nombre = rs.getString("NombreEquipo");
+                equipo.estado = rs.getString("Estado");
+                
+                equipos.add(equipo);
+            }
 
-    public void setDatos(Datos datos) {
-        this.datos = datos;
+            rs.close();
+            
+            return equipos;
+        }catch (Exception e) {
+            System.out.println("No se pudo ejecutar consultarEquipo() a la tabla Equipo" + e);
+        }
+                
+        return null;
     }
 }
